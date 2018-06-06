ebf13ffa61ce845f84af6bde732a7f03421eb05d
sanskarsharma <sanskar2996@gmail.com>
2018-06-07 01:03:49 +0530

    updates

--- src/main/java/githubsearch_cmd/Main.java
+++ src/main/java/githubsearch_cmd/Main.java
@@ -9,6 +9,7 @@ import java.util.StringTokenizer;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Main {
 
@@ -21,6 +22,8 @@ public class Main {
 		
 		String input = "n";
 		Scanner x = new Scanner(System.in);
+		
+		System.out.println("Welcome, This is a command line program for showing github search results. Please press <enter> to continue");
 
 		while(true){
 			
@@ -31,10 +34,29 @@ public class Main {
 			}
 			
 			if(input.equals("n")){
-				x.nextLine();
+				x.nextLine();  
 				System.out.println("Enter search term(s)");
-				String query = x.nextLine();
-				list = getAsList(prepareQueryString(query));
+				String query = prepareQueryString(x.nextLine());
+				
+				Document doc = null;
+				try {
+					
+					doc = Jsoup.connect("https://github.com/search?q="+query).get();
+					
+				} catch (IOException e) {
+					
+					System.out.println("Could not connect to the internet, Please check your internet connection and try again");
+					//e.printStackTrace();
+					continue;
+				}
+				
+				list = getAsList(doc);
+				if( list!=null && list.size()>0){
+					showResults(list);
+				}
+				else{
+					System.out.println("Your search returned zero results, press <enter> for new search");
+				}
 				
 			}else if (input.equals("q")){
 				
@@ -55,11 +77,15 @@ public class Main {
 					System.out.println(model.getRepo_name());
 					System.out.println(model.getRepo_owner());
 					System.out.println(model.getRepo_url());
+					System.out.println(model.getRepo_desc());
+					
 				}else{
 					System.out.println("Invalid serial number, Please enter number from 1 to "+list.size());
 				}
 				
 
+			}else{
+				System.out.println("Invalid choice, please try again");
 			}
 			
 			
@@ -70,29 +96,36 @@ public class Main {
 		
 	}
 	
-	private static ArrayList<SearchResultModel> getAsList(String query){
-				
-		Document doc = null;
-		try {
-			
-			doc = Jsoup.connect("https://github.com/search?q="+query).get();
-			
-		} catch (IOException e) {
-
-			e.printStackTrace();
+	private static void showResults(ArrayList<SearchResultModel> list){
+		
+		int i = 0;
+		while(i<list.size()){
+			System.out.println((i+1)+ ". "+list.get(i).repo_name);
+			i++;
 		}
 		
 		
+	}
+	
+	private static ArrayList<SearchResultModel> getAsList(Document doc){
+				
+				
 		ArrayList<SearchResultModel> list = new ArrayList<SearchResultModel>();
 		
 		Element element = doc.getElementsByClass("repo-list").first();
+		if( element == null){
+			return null;
+		}
+		Elements elements = element.getElementsByClass("repo-list-item");
 		
-		int i = 1;
-		for (Element elem : element.getElementsByClass("repo-list-item")) {
+		for (Element elem : elements) {
 						
 			Element e = elem.getElementsByClass("col-8").first();
 			Element e1 = e.getElementsByTag("h3").first();
 			Element repo_sign = e1.getElementsByTag("a").first();
+			
+			Element one_line = e.getElementsByTag("p").first();
+			String text = one_line.text();
 			
 			String arr[] = repo_sign.text().split("/");
 			
@@ -101,20 +134,14 @@ public class Main {
 			model.setRepo_name(arr[1]);
 			model.setRepo_owner(arr[0]);
 			model.setRepo_url("https://github.com"+repo_sign.attr("href"));
+			model.setRepo_desc(text);
 			
 			list.add(model);
 			
-			System.out.println(i+ ". "+ model.getRepo_name());
-			i++;
-
-			
 		}
 		
-		
-		
-		
-
 		return list;
+		
 	}
 	
 	private static String prepareQueryString(String query){
